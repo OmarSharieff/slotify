6cf52ab7e8516fd1ed3c673eff51162c94239b35
Omar <omarsharief642002@gmail.com>
2025-01-03 15:51:45 +0530

    Add card to create username

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,4 +1,7 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function OnboardingRoute() {
   return (
@@ -8,9 +11,22 @@ export default function OnboardingRoute() {
           <CardTitle>Welcome to Slot<span className="text-primary">ify</span></CardTitle>
           <CardDescription>We need the following information to set up your profile!</CardDescription>
         </CardHeader>
-        <CardContent className="grid gap-y-5 ">
-
+        <CardContent className="flex flex-col gap-y-5">
+          <div className="grid gap-y-2">
+            <Label>Full Name</Label>
+            <Input placeholder="Your name"/>
+          </div>
+          <div className="grid gap-y-2">
+            <Label>Username</Label>
+            <div className="flex rounded-md ">
+              <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-sm text-muted-foreground">slotify.com/</span>
+              <Input placeholder="example-user-1" className="rounded-l-none"/>
+            </div>
+          </div>
         </CardContent>
+        <CardFooter>
+          <Button className="w-full">Submit</Button>
+        </CardFooter>
       </Card>
     </div>
   )
