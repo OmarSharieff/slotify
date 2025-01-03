34ce9551e877585773f137a1eb8e036e7d687857
Omar <omarsharief642002@gmail.com>
2025-01-03 21:15:44 +0530

    add userName to schema.prisma

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
@@ -8,26 +15,37 @@ export default function OnboardingRoute() {
     <div className="min-h-screen w-screen flex items-center justify-center">
       <Card>
         <CardHeader>
-          <CardTitle>Welcome to Slot<span className="text-primary">ify</span></CardTitle>
-          <CardDescription>We need the following information to set up your profile!</CardDescription>
+          <CardTitle>
+            Welcome to Slot<span className="text-primary">ify</span>
+          </CardTitle>
+          <CardDescription>
+            We need the following information to set up your profile!
+          </CardDescription>
         </CardHeader>
-        <CardContent className="flex flex-col gap-y-5">
-          <div className="grid gap-y-2">
-            <Label>Full Name</Label>
-            <Input placeholder="Your name"/>
-          </div>
-          <div className="grid gap-y-2">
-            <Label>Username</Label>
-            <div className="flex rounded-md ">
-              <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-sm text-muted-foreground">slotify.com/</span>
-              <Input placeholder="example-user-1" className="rounded-l-none"/>
+        <form>
+          <CardContent className="flex flex-col gap-y-5">
+            <div className="grid gap-y-2">
+              <Label>Full Name</Label>
+              <Input placeholder="Your name" />
             </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Submit</Button>
-        </CardFooter>
+            <div className="grid gap-y-2">
+              <Label>Username</Label>
+              <div className="flex rounded-md ">
+                <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-sm text-muted-foreground">
+                  slotify.com/
+                </span>
+                <Input
+                  placeholder="example-user-1"
+                  className="rounded-l-none"
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Submit</Button>
+          </CardFooter>
+        </form>
       </Card>
     </div>
-  )
+  );
 }
