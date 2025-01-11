9431000c5c4ccd72e9e299bf98dd1ffcbe680075
Omar <omarsharief642002@gmail.com>
2025-01-11 11:49:46 +0530

    Add EventType Model in prisma schema

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,42 @@
+import { notFound } from "next/navigation";
+import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
+import { EmptyState } from "../CustomComponents/EmptyState";
+
+//fetching data from prisma EventType Model
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      userName: true,
+      eventType: {
+        select: {
+          id: true,
+          active: true,
+          title: true,
+          url: true,
+          duration: true,
+        },
+      },
+    },
+  });
+
+  if (!data) {
+    return notFound();
+  }
+
+  return data;
+}
 
 export default async function DashboardPage() {
-    const session = await requireUser();
+  const session = await requireUser();
+  const data = await getData(session.user?.id as string);
 
-    return(
-        <div>
-            <h1>Hello from the dashboard</h1>
-        </div>
-    )
+  return (
+    <>
+      {data.eventType.length === 0 ? <EmptyState/> : <p>Hey we have data</p>}
+    </>
+  );
 }
